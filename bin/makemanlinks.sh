cb1332702e358b1ccad5daac8f850bbddbae882a
Grant McWilliams <grant@soundlinuxtraining.com>
2013-05-25 14:32:39 -0700

    adds makemanlinks.sh

--- bin/makemanlinks.sh
+++ bin/makemanlinks.sh
@@ -4,16 +4,19 @@ ADOCPATH="../docs/source/asciidoc"
 
 for SUBCOMMAND in $(ssh root@testcloud1 "xe help --all --minimal | sed 's/,/\n/g' | sed -e 's/^[ \t]*//'" | sed '/^$/d')
 do
-	OBJECT=$(echo $SUBCOMMAND | awk -F\- '{print $1}' | tr '[:lower:]' '[:upper:]' )_COMMANDS
-	if [ -L "${ADOCPATH}/RELEASE/xe-${SUBCOMMAND}.ad" ] ;then
-		rm -f "${ADOCPATH}/RELEASE/xe-${SUBCOMMAND}.ad" 
-		mv "${ADOCPATH}/${OBJECT}/xe-${SUBCOMMAND}.ad" "${ADOCPATH}/RELEASE/"
-	else	
-		if [ ! -d "${ADOCPATH}/${OBJECT}" ] ;then
-			mkdir -p "${ADOCPATH}/${OBJECT}"
-		fi		
-		touch "${ADOCPATH}/${OBJECT}/xe-${SUBCOMMAND}.ad" 
-	fi
+        OBJECT=$(echo $SUBCOMMAND | awk -F\- '{print $1}' | tr '[:lower:]' '[:upper:]' )_COMMANDS
+        if [ -L "${ADOCPATH}/RELEASE/xe-${SUBCOMMAND}.ad" ] ;then
+                git rm -f "${ADOCPATH}/RELEASE/xe-${SUBCOMMAND}.ad"         
+                git mv "${ADOCPATH}/${OBJECT}/xe-${SUBCOMMAND}.ad" "${ADOCPATH}/RELEASE/"
+        else    
+                if [ ! -d "${ADOCPATH}/${OBJECT}" ] ;then
+                        mkdir -p "${ADOCPATH}/${OBJECT}"
+                fi              
+                touch "${ADOCPATH}/${OBJECT}/xe-${SUBCOMMAND}.ad"             
+                git add "${ADOCPATH}/${OBJECT}/xe-${SUBCOMMAND}.ad"
+                git commit -m "Adds xe-${SUBCOMMAND}.ad"
+        fi
 read blank
+
 done
 
