30f5c4c55a077f1d192760b486e2e37c055e6b97
Grant McWilliams <grant@soundlinuxtraining.com>
2013-06-23 20:15:18 -0700

    Merges listmanpagemaker and manpagemaker. Also adds quite a lot of functionality for manpagemaker.sh

--- bin/manpagemaker.sh
+++ bin/manpagemaker.sh
@@ -1,95 +1,12 @@
 #!/bin/bash
-#12/25/2012 .
-#set -x
-#
+#12/25/2012 - Matthew Spah
+#06/25/2013 - Grant McWilliams
+
 oIFS=$IFS
 IFS="\n"
 
-XE_COMMAND=$1
-HELPDISPLAY=$(xe help "$XE_COMMAND")
-
-
-# I would like to turn oparams and reparams into arrays
-oparams(){
-	IFS=","
-	local LIST=$(echo "$HELPDISPLAY" | grep "optional params" | sed -e 's/optional params//' | sed -e 's/^[ \t]*//' | sed -e 's/://' | sed 's/^[ \t]*//')
-	# grepping the xe help display and creating a listwith its contents
-	for param in $LIST
-	do
-		local PARAMSTRING=$(echo "$param" | sed -e 's/^[ \t]*//')
-		echo "*$PARAMSTRING*::"
-		echo ""
-	done ; IFS=$oIFS
-}
-
-reparams(){
-	# grepping the xe help display and creating a list with its contents
-	local LIST=$(echo "$HELPDISPLAY" | grep "reqd params" | sed -e 's/reqd params//' | sed -e 's/^[ \t]*//' | sed -e 's/://' | sed 's/^[ \t]*//')
-	IFS=","
-	for param in $LIST
-	do	
-		local PARAMSTRING=$(echo "$param" | sed -e 's/^[ \t]*//')
-		echo "*$PARAMSTRING*::"
-		echo ""
-	done ; IFS=$oIFS
-}
-
-
-createpage()
+createheader()
 {
-	echo "XE(1)"
-	echo "======="
-	echo ":doctype: manpage"
-	echo ":man source:   xe $XE_COMMAND"
-	echo ":man version:  {1}"
-	echo ":man manual:   xe $XE_COMMAND manual"	
-	#
-	echo ""
-	echo "NAME"
-	echo "----"
-	echo ""
-	echo "$XE_COMMAND -"
-	echo ""
-	echo "SYNOPSIS"
-	echo "--------"
-	echo ""
-	echo "DESCRIPTION"
-	echo "-----------"
-	echo ""
-	echo "REQUIRED PARAMETERS"
-	echo "-------------------"
-	echo ""
-	# required params
-	reparams
-	echo "OPTIONAL PARAMETERS"
-	echo "-------------------"
-	echo ""
-	# optional params
-	oparams
-	echo "EXAMPLES"
-	echo "--------"
-	echo ""
-	echo "SEE ALSO" # Parsing a database of XE subcommands using pattern matching for the see also section
-	echo "--------"
-	echo ""
-	echo "AUTHORS"
-	echo "-------"
-	echo "Manpage Author(s): ::"
-	echo "Example User <username@mail.com>" # add user input options for authors
-	echo ""
-	echo "BUGS"
-	echo "----"
-	echo "See http://wiki.xen.org/wiki/Reporting_Bugs_against_XCP on how to send bug reports, Send bugs to xen-devel@lists.xen.org. General questions can be sent to xen-api@lists.xen.org."
-	echo ""
-	echo ""
-	echo "COPYRIGHT"
-	echo "---------"
-	echo "Copyright \(C) 2013 - Example Name"
-}
-
-createlistpage()
-{
-	# Building the heading
 	echo "XE(1)"
 	echo "======="
 	echo ":doctype: manpage"
@@ -97,57 +14,141 @@ createlistpage()
 	echo ":man version:  {1}"
 	echo ":man manual:   xe $XE_COMMAND manual"
 	echo ""
-	echo "NAME"
-	echo "-----"
-	echo "xe-$XE_COMMAND - Displays  parameters."	
-	echo ""
-	echo "SYNOPSIS"
-	echo "--------"
-	echo "*xe $XE_COMMAND*  [ params=<PARAMETER> ] [--minimal]"
+}
+
+createdescription()
+{
 	echo ""
 	echo "DESCRIPTION"
 	echo "-----------"
-	echo "*xe $XE_COMMAND* displays <OBJECT> and their parameters."
+	echo ""	
+}
+
+createlistdescription()
+{
+	echo ""
+	echo "DESCRIPTION"
+	echo "-----------"
+	echo ""	
+	echo "*xe ${XE_COMMAND}* displays ${OBJECTUPPER}s and their parameters."
 	echo ""
 	echo "Output can be filtered by using the *params* parameter and a value (separate multiple parameters with commas): ::"
-	echo "	*xe $XE_COMMAND params=<ONE PARAMETER>* +"
-	echo "	*xe $XE_COMMAND params=<MULTIPLE PARAMETERS>*"
+	echo "	*xe ${XE_COMMAND} params=<PARAM1>* +"
+	echo "	*xe ${XE_COMMAND} params=<PARAM1,PARAM2,PARAM3>*"
 	echo ""
 	echo "Output can be filtered by using parameter values and the desired value: ::"
-	echo "	*xe $XE_COMMAND <PARAMETER>=<VALUE>*"
+	echo "	*xe ${XE_COMMAND} <PARAMETER>=<VALUE>*"
 	echo ""
 	echo "Append --minimal to display values for one parameter on a single line separated by commas: ::"
-	echo "	*xe $XE_COMMAND params=\"<PARAMETER>\" --minimal*"
+	echo "	*xe ${XE_COMMAND} params=\"<PARAMETER>\" --minimal*"
 	echo ""
-	echo "OPTIONAL PARAMETERS"
-	echo "-------------------"
-	echo "*all*::"
-	echo "	Display all parameter values"
+}
+
+createparams(){
 	echo ""
-	oparams
+	PARAMTYPES="reqd optional"
+	for TYPE in $PARAMTYPES ;do
+		PARAMS=$(xe help "$XE_COMMAND" | grep "$TYPE params" | sed -e 's/^.*optional params[ ]:[ ]//')
+		if [[ -z "$PARAMS" ]] ;then
+			continue
+		fi
+		case $TYPE in
+			"reqd") 
+				echo "REQUIRED PARAMETERS"
+				echo "-------------------"
+				echo ""
+			;;
+		"optional") 
+				echo "OPTIONAL PARAMETERS"
+				echo "-------------------"
+				echo "*all*::"
+				echo "	Display all parameter values"
+				echo ""
+			;;
+		esac
+		IFS=","
+		for param in $PARAMS ;	do
+			local PARAMSTRING=$(echo "$param" | sed -e 's/^[ \t]*//')
+			case $PARAMSTRING in
+				"params") echo -e "*$PARAMSTRING*:: \n\tParameter(s) to filter on" ;;
+				"uuid") echo -e "*$PARAMSTRING*:: \n\t${OBJECTUPPER} UUID" ;;
+				"name-label") echo -e "*$PARAMSTRING*:: \n\t${OBJECTUPPER} label" ;;
+				"name-description") echo -e "*$PARAMSTRING*:: \n\t${OBJECTUPPER} description" ;;
+				"other-config") echo -e "*$PARAMSTRING*:: \n\t${OBJECTUPPER} other-config parameters" ;;
+				"tags") echo -e "*$PARAMSTRING*:: \n\t${OBJECTUPPER} tags" ;;
+				*) echo -e "*$PARAMSTRING*:: \n" ;;
+			esac
+		done
+		case $TYPE in
+			"optional") 
+				echo ""
+				echo "*--minimal*::"
+				echo "	Specify --minimal to only show minimal output"
+			;;
+		esac
+	done
+}
+
+createpage()
+{
 	echo ""
-	echo "*--minimal*::"
-	echo "	Specify --minimal to only show minimal output"
+	echo "NAME"
+	echo "----"
+	echo ""
+	echo "${XE_COMMAND} -"
+	echo ""
+	echo "SYNOPSIS"
+	echo "--------"
+	createdescription
+	createparams
+	echo "EXAMPLES"
+	echo "--------"
+	echo ""
+	createseealso
+}
+
+createlistpage()
+{
+
+	echo "NAME"
+	echo "-----"
+	echo "xe-${XE_COMMAND} - Displays ${OBJECTUPPER}s"	
+	echo ""
+	echo "SYNOPSIS"
+	echo "--------"
+	echo "*xe ${XE_COMMAND}*  [ params=<PARAMETER> ] [--minimal]"
+	createlistdescription
+	createparams 
 	echo ""
 	echo "EXAMPLES"
 	echo "--------"
-	echo "To display all parameters for all *: ::"
-	echo "	*xe <OBJECT>-list* params=\"all\""
-	echo "To display all paramters for a specific *: ::"
-	echo "	*xe <OBJECT>-list* uuid=<OBJECT UUID> params=\"all\""
-	echo "To display <PARAMETER> for a specific <OBJECT>: ::"
-	echo "	*xe <OBJECT>-list* uuid=<NETWORK UUID> params=\"VIF-uuids\""
-	echo "To display the <PARAMETER> and <PARAMETER> parameters of all <OBJECT> with a <PARAMETER> of <VALUE>: ::"
-	echo "	*xe <OBJECT>-list* <PARAMETER>=<VALUE> params=\"<MULTIPLE PAMETERS>\""
+	echo "To display all parameters for all ${OBJECTUPPER}s *: ::"
+	echo "	*xe ${XE_COMMAND}* params=\"all\""
+	echo "To display all parameters for a specific ${OBJECTUPPER} *: ::"
+	echo "	*xe ${XE_COMMAND}* uuid=<${OBJECT} UUID> params=\"all\""
+	echo "To display <PARAMETER> for a specific ${OBJECTUPPER} : ::"
+	echo "	*xe ${XE_COMMAND}* uuid=<${OBJECT} UUID> params=\"VIF-uuids\""
+	echo "To display the <PARAMETER> and <PARAMETER> parameters of all ${OBJECTUPPER}s  with a <PARAMETER> of <VALUE>: ::"
+	echo "	*xe ${XE_COMMAND}* <PARAMETER>=<VALUE> params=\"<MULTIPLE PAMETERS>\""
+	createseealso
+}
+
+createseealso()
+{
+	
 	echo ""
 	echo "SEE ALSO"
 	echo "--------"
-	echo "*xe help <OBJECT>-list*,"
+	echo "*xe help ${XE_COMMAND}*,"
 	echo ""
+}
+
+createfooter()
+{
 	echo "AUTHORS"
 	echo "-------"
 	echo "Manpage Author(s): ::"
-	echo "Example User <username@mail.com>" # add user input options for authors
+	echo "$COPYNAME $COPYEMAIL" # add user input options for authors
 	echo ""
 	echo "BUGS"
 	echo "----"
@@ -155,14 +156,33 @@ createlistpage()
 	echo ""
 	echo "COPYRIGHT"
 	echo "---------"
-	echo "Copyright \(C)  2013 - <manpage author name>"
+	echo "Copyright \(C)  $(date +"%Y") - $COPYNAME"
 	echo "Permission is granted to copy, distribute and/or modify this document under the terms of the GNU Free Documentation License, Version 1.3 or any later version published by the Free Software Foundation; with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts. A copy of the license is included in the section entitled \"GNU Free Documentation License\""
 }
 
-if echo "$XE_COMMAND" | grep -e '.*-list$' ;then
+XE_COMMAND="$1"
+HELPDISPLAY=$(xe help "$XE_COMMAND")
+
+OBJECT=${XE_COMMAND%-*}
+OBJTYPE=${XE_COMMAND#*-}
+OBJECTUPPER=$(echo $OBJECT | tr 'a-z' 'A-Z')
+
+# To automate the insertion of copywrite information put the following two lines
+# with your information in $HOME/.manpagemaker_creds.txt 
+COPYEMAIL="<manpage author email address>"
+COPYNAME="<manpage author name>"
+
+if [[ -e "$HOME/.manpagemaker_creds.txt" ]] ;then
+	source "$HOME/.manpagemaker_creds.txt"
+fi
+
+createheader
+if [[ $OBJTYPE = "list" ]] ;then
 	createlistpage
 else
 	createpage
 fi
+createfooter
+
 
 
